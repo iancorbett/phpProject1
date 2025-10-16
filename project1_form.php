1fd8cc60d8e24640c149c976c1d1ff8cece94236
iancorbett <iancorbett1324@gmail.com>
2025-10-15 17:05:02 -0700

    handle post requests for first and lst name

--- project1_form.php
+++ project1_form.php
@@ -3,5 +3,6 @@
 function clean($v){ return trim($v ?? ''); } //TAKES IN VALUE AND TRIMS IT, if null it defaults to an empty string
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-
+    $first = clean($_POST['first_name'] ?? '');
+    $last  = clean($_POST['last_name'] ?? '');
 }
