5c8d959f428972437d0e818fd14c504188a25abe
iancorbett <iancorbett1324@gmail.com>
2025-10-16 14:17:47 -0700

    create title as well as nav link back to inputform

--- project1_view.php
+++ project1_view.php
@@ -8,9 +8,13 @@ $csvPath = __DIR__ . '/data/form_submissions1.csv';
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Project-1: CSV Viewer</title>
 </head>
 <body>
+<h1>Project #1 – CSV Viewer</h1>
+  <nav>
+     <a href="project1_form.php">Back to Form</a>
+  </nav>
     
 </body>
 </html>
