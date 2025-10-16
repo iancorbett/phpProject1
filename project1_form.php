cd1e2f776ec434cbe106b1d984c09fbc901ebaa6
iancorbett <iancorbett1324@gmail.com>
2025-10-15 17:06:04 -0700

    handle post requests for city, state, and zip code

--- project1_form.php
+++ project1_form.php
@@ -5,4 +5,8 @@ function clean($v){ return trim($v ?? ''); } //TAKES IN VALUE AND TRIMS IT, if n
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $first = clean($_POST['first_name'] ?? '');
     $last  = clean($_POST['last_name'] ?? '');
+    $street= clean($_POST['street'] ?? '');
+    $city  = clean($_POST['city'] ?? '');
+    $state = clean($_POST['state'] ?? '');
+    $zip   = clean($_POST['zip'] ?? '');
 }
